25219bbc0db6f38248de9f25b572ab171875ad77
Amber VanGieson <ambervangieson@gmail.com>
2021-05-09 18:42:41 -0400

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const promptUser = () =>
         {
             type: 'list',
             message: 'What would you like to do?',
-            choices: ['View All Employees', 'View All Employees By Department', 'View All Employees By Manager', 'Add Employee', 'Remove Employee', 'Update Employee Role',  'Add New Role', 'Exit'],
+            choices: ['View All Employees', 'View All Employees By Department', 'View All Employees By Manager', 'Add Employee', 'Update Employee Role',  'Add New Role', 'Exit'],
             name: 'initialPrompt'
         }
     ])
@@ -33,14 +33,8 @@ const promptUser = () =>
             case 'Add Employee':
                 addEmployee()
                 break
-            case 'Remove Employee':
-                removeEmployee()
-                break
             case 'Update Employee Role':
                 updateEmployeeRole()
-                break
-            case 'Add New Department':
-                addNewDepartment()
                 break
             case 'Add New Role':
                 addNewRole()
